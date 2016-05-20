8744db309dceb63cee7db849e425805883f0b9bb
Primesty <matthias.raess@gmail.com>
2016-05-20 17:57:19 -0400

    Solution to Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+### Coursera - RProgramming - Assignment 2
+
+# The two functions below make use of the <<- operator 
+
+## The function below makes it possible to cache a matrix object to potentially save time from a 
+# computational standpoint
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inverse <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## The second function creates an inverse for the matrix object created in the first function ONLY if, 
+# the matrix has not already been inversed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  mat <- x$get()
+  inverse <- solve(mat, ...)
+  x$setInverse(inverse)
+  inverse
 }
